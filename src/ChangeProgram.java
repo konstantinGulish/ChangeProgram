afeea43ac06d6f89ee0fb6f56e6d940c08e8a11b
konstantinGulish <konstantin_gulish@yahoo.com>
2018-05-31 12:52:57 -0400

    initial commit

---
+++ src/ChangeProgram.java
@@ -0,0 +1,10 @@
+public class ChangeProgram {
+    public static void main(String[] args) {
+        int x;
+        x = 5;
+        double y = 9.5;
+        double z = x * y;
+
+        System.out.println("The product is " + z);
+    }
+}
